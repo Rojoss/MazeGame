58fca68bf3619ce134c86b0a856f80e4d103598e
Jos <josroossien@hotmail.com>
2016-05-16 16:01:39 +0200

    Base popup script and riddle popup.

---
+++ Assets/Scripts/UI/Popup.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using System.Collections;
+
+public abstract class Popup : MonoBehaviour {
+
+    private static Popup current = null;
+    public RigidbodyFirstPersonController playerController;
+
+    public void Open() {
+        if (isOpen()) {
+            return;
+        }
+        playerController.enabled = false;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        if (current != null) {
+            current.Close();
+        }
+        current = this;
+        current.gameObject.SetActive(true);
+        onOpen();
+    }
+
+    public void Close() {
+        playerController.enabled = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        if (current == null) {
+            return;
+        }
+        current.gameObject.SetActive(false);
+        current = null;
+        onClose();
+    }
+
+    public bool isOpen() {
+        if (current == null) {
+            return false;
+        }
+        return current.Equals(this);
+    }
+
+    public abstract void onOpen();
+
+    public abstract void onClose();
+}
---
+++ Assets/Scripts/UI/RiddlePopup.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class RiddlePopup : Popup {
+
+    public Text riddleText;
+    public InputField answerInput;
+
+    private Riddle riddle;
+
+    public static RiddlePopup Instance { get; private set; }
+
+    public void Awake() {
+        if (Instance != null) {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        gameObject.SetActive(false);
+    }
+
+    public void setRiddle(Riddle riddle) {
+        this.riddle = riddle;
+        riddleText.text = riddle.riddle;
+    }
+
+    public string getInput() {
+        return answerInput.text;
+    }
+
+    public void onUpdateInput() {
+        
+    }
+
+    public void onSubmitInput() {
+        if (riddle != null) {
+            riddle.onSubmit(getInput());
+        }
+    }
+
+    public void reset() {
+        riddleText.text = "???";
+        answerInput.text = "";
+    }
+
+    public void resetInput() {
+        answerInput.text = "";
+    }
+
+    public override void onOpen() {
+        answerInput.Select();
+        answerInput.ActivateInputField();
+    }
+
+    public override void onClose() {
+        answerInput.DeactivateInputField();
+        reset();
+    }
+}
