3f3dc5ee6c0ab7b43f51bd89c72c3f5b3c7e22e0
Jos <josroossien@hotmail.com>
2016-05-16 16:02:48 +0200

    Riddle script and temp material Note: Riddles work but they don't indicate a direction yet after submitting an answer.

---
+++ Assets/Scripts/Riddles/Riddle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Riddle : MonoBehaviour {
+
+    public string riddle;
+    public string[] answers;
+
+    void OnMouseDown() {
+        RiddlePopup.Instance.setRiddle(this);
+        RiddlePopup.Instance.Open();
+    }
+
+    public void onSubmit(string answer) {
+        RiddlePopup.Instance.Close();
+        answer = answer.ToLower().Trim();
+        if (answer.Length == 0) {
+            return;
+        }
+        for (int i = 0; i < answers.Length; i++) {
+            if (answers[i].Equals(answer)) {
+                onAnswer(true);
+                return;
+            }
+        }
+        onAnswer(false);
+    }
+
+    private void onAnswer(bool correct) {
+        Debug.Log("Answer is " + (correct ? "correct" : "incorrect") + "!");
+        //TODO: Show direction
+    }
+}
